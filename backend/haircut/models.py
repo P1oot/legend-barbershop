1b5b9a844d6b203051db6812a0a22afcbb383549
Dan <dan.berdinskikh@mail.ru>
2023-09-12 15:50:58 +0300

    Add services model

--- backend/haircut/models.py
+++ backend/haircut/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Services(models.Model):
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=200,
+        unique=True,
+    )
+    price = models.PositiveIntegerField(
+        verbose_name='Цена',
+        help_text='руб.',
+    )
+    description = models.CharField(
+        verbose_name='Описание',
+    )
+
+    class Meta:
+        verbose_name = 'Услуга'
+        verbose_name_plural = 'Услуги'
+
+    def __str__(self) -> str:
+        return self.name
