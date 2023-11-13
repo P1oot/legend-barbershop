c1b6cce85d501640826374c9c368510649315283
Dan <dan.berdinskikh@mail.ru>
2023-11-13 19:44:27 +0300

    Fix admin output

--- backend/haircut/admin.py
+++ backend/haircut/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 
 from .models import Services, Haircuts, Promotions
 
-admin.site.register(Services)
+
+@admin.register(Services)
+class ServicesAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+
+
+@admin.register(Promotions)
+class PromotionsAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
 admin.site.register(Haircuts)
-admin.site.register(Promotions)
